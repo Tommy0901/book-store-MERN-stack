98f7b1487722a4f53ff944f4fc22af8963e295a1
Tommy0901 <ak710180@gmail.com>
2024-02-17 11:06:46 +0800

    feat: create our first Http Route

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Lessons
 
 - [x] Lesson 01: Create Node.js project from scratch
+- [x] Lesson 02: Create our first Http Route
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import { port } from './config.js'
 
 const app = express()
 
+app.get('/', (req, res) => res.status(234).send('Welcome To MERN Stack Tutorial'))
+
 app.listen(port, () => {
   console.log(`Express server is running on http://localhost:${port}`)
 })
