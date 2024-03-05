957c6a37795104c4728adc0cc62fcf8fab26267d
Tommy0901 <ak710180@gmail.com>
2024-03-05 09:47:58 +0800

    feat: save a new Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from 'express'
 import { PORT, ATLAS_URI } from './config.js'
 import mongoose from 'mongoose'
+import { Book } from './models/bookModel.js'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.status(234).send('Welcome To MERN Stack Tutorial'))
+
+app.post('/books', async (req, res) => {
+  try {
+    const { body: { title, author, publishYear } } = req
+    if (!title || !author || !publishYear) return res.status(401).send({ message: 'Send all required fields: title, author, publishYear' })
+    return res.status(201).send(await Book.create({ title, author, publishYear }))
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).send({ message: err.message })
+  }
+})
 
 mongoose
   .connect(ATLAS_URI)
