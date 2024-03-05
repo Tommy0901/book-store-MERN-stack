ee4b6f80d4c011d16c0579fdf7586260811d8540
Tommy0901 <ak710180@gmail.com>
2024-03-05 14:49:47 +0800

    feat: update a Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.post('/books', async (req, res) => {
   try {
     const { body: { title, author, publishYear } } = req
     const ifCond = !title || !author || !publishYear
-    if (ifCond) return res.status(401).send({ message: 'Send all required fields: title, author, publishYear' })
+    if (ifCond) return res.status(401).json({ message: 'Send all required fields: title, author, publishYear' })
     res.status(201).json(await Book.create({ title, author, publishYear }))
   } catch (err) {
     console.log(err.message)
@@ -38,6 +38,20 @@ app.get('/books/:id', async (req, res) => {
   try {
     const { id } = req.params
     res.status(200).json(await Book.findById(id))
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.put('/books/:id', async (req, res) => {
+  try {
+    const { params: { id }, body: { title, author, publishYear } } = req
+    const ifCond = !title || !author || !publishYear
+    if (ifCond) return res.status(401).send({ message: 'Send all required fields: title, author, publishYear' })
+    const result = await Book.findByIdAndUpdate(id, { title, author, publishYear })
+    if (!result) return res.status(404).json({ message: 'Book not found' })
+    res.status(200).json({ message: 'Book updated successfully' })
   } catch (err) {
     console.log(err.message)
     res.status(500).json({ message: err.message })
